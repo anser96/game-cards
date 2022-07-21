799f4216bb98c47fe907fa8c5a2deb25370794af
Andres Saenz <asaenzcordero@gmail.com>
2022-07-20 22:38:25 -0500

    ajustes adapter

--- infrastructure/adapters/src/main/java/org/example/adapters/MongoRepositoryAdapter.java
+++ infrastructure/adapters/src/main/java/org/example/adapters/MongoRepositoryAdapter.java
@@ -1,15 +1,15 @@
 package org.example.adapters;
 
+import co.com.game.model.Card;
+import co.com.game.model.gateway.CardRepository;
 import org.example.adapters.helper.OperationAdapter;
-import co.com.sofka.model.Pet;
-import co.com.sofka.model.gateway.PetRepository;
 import org.reactivecommons.utils.ObjectMapperI;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class MongoRepositoryAdapter extends OperationAdapter<Pet, CardDocument, String, MongoDBRepository> implements PetRepository {
+public class MongoRepositoryAdapter extends OperationAdapter<Card, CardDocument, String, MongoDBRepository> implements CardRepository {
 
     public MongoRepositoryAdapter(MongoDBRepository mongoDBRepository, ObjectMapperI mapper) {
-        super(mongoDBRepository, mapper, d -> mapper.map(d, Pet.class));
+        super(mongoDBRepository, mapper, d -> mapper.map(d, Card.class));
     }
 }
