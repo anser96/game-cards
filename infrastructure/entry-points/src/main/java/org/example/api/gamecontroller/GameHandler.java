1754c47983ace18f42568b4b8097d31ab3e607b7
Andres Saenz <asaenzcordero@gmail.com>
2022-07-24 22:38:59 -0500

    deploy

--- infrastructure/entry-points/src/main/java/org/example/api/gamecontroller/GameHandler.java
+++ infrastructure/entry-points/src/main/java/org/example/api/gamecontroller/GameHandler.java
@@ -1,17 +1,16 @@
 package org.example.api.gamecontroller;
 
 
-import co.com.game.model.Card;
+
 import co.com.game.model.Game;
-import co.com.game.model.Player;
+
 import co.com.game.model.event.DomainEvent;
-import com.google.gson.Gson;
-import com.sun.tools.jconsole.JConsoleContext;
+
 import lombok.RequiredArgsConstructor;
 import org.example.adapters.event.EventPublisher;
 import org.example.usecase.game.CreateGameUseCase;
 import org.example.usecase.game.StartGameUseCase;
-import org.json.JSONObject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -20,7 +19,7 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
-import java.util.stream.Collectors;
+
 
 @Component
 @RequiredArgsConstructor
@@ -40,7 +39,7 @@ public class GameHandler {
 
     public Mono<ServerResponse> startGame(ServerRequest serverRequest) {
         String gameId = serverRequest.pathVariable("gameId");
-        var request = startGameUseCase.gameById(gameId)
+        Mono<Game> request = startGameUseCase.gameById(gameId)
                 .flatMap(startGameUseCase::startGame);
 
         request.subscribe(publisher::publish);
