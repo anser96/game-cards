29b0c62925bf54ea192e28e7d58df57cc9e7ca84
Andres Saenz <asaenzcordero@gmail.com>
2022-07-25 00:06:54 -0500

    player repository

--- domain/model/src/main/java/co/com/game/model/Player.java
+++ domain/model/src/main/java/co/com/game/model/Player.java
@@ -11,12 +11,13 @@ public class Player {
     private String name;
     private String email;
     private Double points;
+    private Set<Card> deck;
 
     public Player() {
         this.name = null;
         this.email = null;
         this.points = 0.0;
-
+        this.deck = Set.of();
     }
 
     public Player(String id, String name, String email) {
@@ -24,6 +25,6 @@ public class Player {
         this.name = name;
         this.email = email;
         this.points = 0.0;
-
+        this.deck = Set.of();
     }
 }
